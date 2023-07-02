b45864de881c80536269b3ef3afc473567a0634d
Jason Back <jason.back01@gmail.com>
2023-07-02 19:37:39 -0400

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    // create array of messages
+    // takes in array of names
+    // create message with each name
+    // add each message to the messages array
+    // return messages array
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return messages
+}
+
+function countDown() {
+    for (let i = 10; i >= 0; i--) {
+    console.log(i);
+    }
+}
